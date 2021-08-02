61d362677052e69652787ffe109f828bd8647601
Simonbelete <simonbelete@gmail.com>
2021-08-02 13:16:30 +0300

    adding dark mode for logo atom

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 module.exports = {
-  purge: [],
-  darkMode: "media", // or 'media' or 'class'
+  mode: "jit",
+  // These paths are just examples, customize them to match your project structure
+  purge: ["./public/**/*.html", "./components/**/*.{js,jsx,ts,tsx,vue}"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        primary: "F8D57E",
-        secondary: "F6F7FB",
-        "dark-primary": "121212",
+        primary: "#F8D57E",
+        secondary: "#F6F7FB",
+        "dark-primary": "#121212",
+        "dark-secondary": "#dedede",
       },
     },
   },
